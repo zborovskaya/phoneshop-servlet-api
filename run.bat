0dd843e8e3dad0734ea05868e747b7f209febce6
Andrei Osipenko <andrei.osipenko@expert-soft.by>
2019-07-10 16:27:55 +0300

    Added run-war comments

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 set MAVEN_OPTS=-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000
+rem mvn -Dmaven.test.skip=true tomcat7:run-war
 mvn tomcat7:run
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export MAVEN_OPTS=-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000
+# mvn -Dmaven.test.skip=true tomcat7:run-war
 mvn tomcat7:run
